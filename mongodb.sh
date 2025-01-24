5467b16a8f3e85686cfe2a414bfd9bfc57e0023c
GOUTHAM <goutamreddy817@gmail.com>
2025-01-24 11:48:32 +0530

    updated

--- mongodb.sh
+++ mongodb.sh
@@ -6,9 +6,3 @@ systemctl start mongod
 systemctl restart mongod
 
 
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-dnf install mongodb-org -y
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
