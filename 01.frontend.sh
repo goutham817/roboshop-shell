163fe0c18f0b0f2ff2469c95553322283524e765
GOUTHAM <goutamreddy817@gmail.com>
2025-01-26 13:27:49 +0530

    updated

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -3,13 +3,15 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+
 # upadte all ip's in nginx.conf file
 cp nginx.conf /etc/nginx/nginx.conf
+
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cd
+
 systemctl restart nginx
 systemctl enable nginx
 
